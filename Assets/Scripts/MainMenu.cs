c0a922c4bfe87062241f987580b66b93f80c1d0a
472529 <472529@chrome.escg.ac.uk>
2020-11-10 16:40:17 +0000

    more work on menus - trying to add a fade in and out of scenes to make the transition not jarring

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
-    public void MainMenu()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    public Animator transition;
+
+    public float transitionTime = 1f;
+    
+    public bool buttonClicked = false;
+
+    void Update(){
+        if (buttonClicked == true)
+        {
+           LoadNextLevel();
+           buttonClicked = false;
+        }
     }
 
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadNextLevel(){
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+
+    }
+
+    public void ButtonClicked(PointerEventData pointerEventData){
+        if(pointerEventData.button == PointerEventData.InputButton.Right){
+            buttonClicked = true;
+        }
     }
 }
