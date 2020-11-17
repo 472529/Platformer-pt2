aa7168cdf8a221b093c2a04c7189196c431528b3
472529 <472529@chrome.escg.ac.uk>
2020-11-17 12:15:41 +0000

    ayooo collision for coins working

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+    public float Gems = 0;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Collectable")
+        {
+            Gems += 1;
+            Destroy
+        }
+    }
+}
