a56babaa4cd67a2ac4ae6f77d2401defa3067529
472529 <472529@chrome.escg.ac.uk>
2020-12-14 00:56:35 +0000

    bit of level design

--- Assets/Scripts/RespawnPlayer.cs
+++ Assets/Scripts/RespawnPlayer.cs
@@ -6,11 +6,15 @@ public class RespawnPlayer : MonoBehaviour
 {
     public float threshold = -15f;
 
+    public Player player;
+
     void FixedUpdate()
     {
         if (transform.position.y < threshold)
         {
             transform.position = new Vector3(-15, 7, 0);
+            player.Health -= 1;
+            player.PlayerHealth();
         }
     }
 }
